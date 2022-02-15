9be0204fefab604fc6604e4c9662d1f5831b9345
brightsummit.co <naseemoyebola@gmail.com>
2022-02-15 23:51:07 +0100

    initial commit

---
+++ VoiceOver/VoiceOver/VoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOverApp.swift
+//  VoiceOver
+//
+//  Created by naseem on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
