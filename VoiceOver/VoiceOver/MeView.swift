53756ef503a0fa9f888f1c2a5b3da0037c750446
brightsummit.co <naseemoyebola@gmail.com>
2022-02-17 01:15:39 +0100

    data persistence + daily notification

--- VoiceOver/VoiceOver/MeView.swift
+++ VoiceOver/VoiceOver/MeView.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Ayindeololade"
     @State private var emailAddress = "naseemoyebola@gmail.com"
-    
+    @State private var qrCode = UIImage()
+
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
@@ -26,13 +27,25 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
-                    .resizable()
+                Image(uiImage: qrCode)
                     .interpolation(.none)
+                    .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            // save my code
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your name")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
     }
     
@@ -44,8 +57,11 @@ struct MeView: View {
                 return UIImage(cgImage: cgimg)
             }
         }
-
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
